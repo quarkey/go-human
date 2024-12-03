6a429c2aabf32638406a70b17b3909820b5fb795
Steffen Lundin <steffen.sandvold@gmail.com>
2024-12-03 12:50:02 +0100

    initial commit

---
+++ bytes_test.go
@@ -0,0 +1,43 @@
+package human_test
+
+import (
+	"testing"
+
+	"github.com/quarkey/go-human"
+)
+
+func TestBytesToHuman(t *testing.T) {
+	tests := []struct {
+		b int64
+		h string // wanted
+	}{
+		{1024, "1.00KB"},
+		{10737418240, "10.00GB"},
+		{1099511627776, "1.00TB"},
+	}
+	for _, c := range tests {
+		got := human.BytesToHuman(c.b)
+		if got != c.h {
+			t.Errorf("BytesToHuman(%d) got == %q want, %q", c.b, got, c.h)
+		}
+		t.Logf("BytesToHuman(%d) got == %q want, %q", c.b, got, c.h)
+	}
+
+}
+
+func TestToBytes(t *testing.T) {
+	tests := []struct {
+		b float64
+		h string
+	}{
+		{1024.00, "1.00KB"},
+		{16976459532861.439453, "15.44TB"},
+	}
+	for _, c := range tests {
+		got, err := human.ToBytes(c.h)
+		if got != c.b || err != nil {
+			t.Errorf("ToBytes(%s) got == %g want, %g", c.h, got, c.b)
+		}
+		t.Logf("ToBytes(%s) got == %g want, %g", c.h, got, c.b)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/quarkey/go-human
+
+go 1.23.2
